c7734ec67cf24632abda4bc23b4f95c07a721f66
Lucas Moura Pereira <lucasmoura@ioasys.com.br>
2022-07-18 11:17:36 -0300

    about

--- react-app/react/CashbackParticipation.tsx
+++ react-app/react/CashbackParticipation.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import { IconMenu } from 'vtex.store-icons'
+import { IconPlus } from 'vtex.store-icons'
 import { useCssHandles } from 'vtex.css-handles'
 
-const CSS_HANDLES = ['wrapperCashbackInfo', 'title', 'wrapperIconList']
+const CSS_HANDLES = [
+  'wrapperCashbackInfo',
+  'title',
+  'wrapperIconList',
+  'wrapperIcon',
+  'IconTitle',
+  'IconText',
+]
 
 const CashbackParticipation: StorefrontFunctionComponent = () => {
   const handles = useCssHandles(CSS_HANDLES)
@@ -11,40 +18,40 @@ const CashbackParticipation: StorefrontFunctionComponent = () => {
     <div className={`${handles.wrapperCashbackInfo}`}>
       <h2 className={`${handles.title}`}>Veja como é fácil participar</h2>
       <div className={`${handles.wrapperIconList}`}>
-        <div>
-          <IconMenu />
-          <p>COMPRE</p>
-          <span>
+        <div className={`${handles.wrapperIcon}`}>
+          <img src="" alt="" />
+          <p className={`${handles.IconTitle}`}>COMPRE</p>
+          <span className={`${handles.IconText}`}>
             A cada um <strong>R$ 1</strong> em produtos
           </span>
         </div>
         <div>
-          <img src="" alt="" />
+          <IconPlus />
         </div>
-        <div>
+        <div className={`${handles.wrapperIcon}`}>
           <img src="" alt="" />
-          <p>GANHE</p>
-          <span>
+          <p className={`${handles.IconTitle}`}>GANHE</p>
+          <span className={`${handles.IconText}`}>
             <strong>1</strong> ponto
           </span>
         </div>
         <div>
-          <img src="" alt="" />
+          <IconPlus />
         </div>
-        <div>
+        <div className={`${handles.wrapperIcon}`}>
           <img src="" alt="" />
-          <p>TROQUE</p>
-          <span>
+          <p className={`${handles.IconTitle}`}>TROQUE</p>
+          <span className={`${handles.IconText}`}>
             Por outros<strong>produtos ou serviços</strong> em lojas parceiras
           </span>
         </div>
         <div>
-          <img src="" alt="" />
+          <IconPlus />
         </div>
-        <div>
+        <div className={`${handles.wrapperIcon}`}>
           <img src="" alt="" />
-          <p>CADASTRE</p>
-          <span>
+          <p className={`${handles.IconTitle}`}>CADASTRE</p>
+          <span className={`${handles.IconText}`}>
             Para acessar a <strong>sua conta</strong> e veja seus pontos
           </span>
         </div>
